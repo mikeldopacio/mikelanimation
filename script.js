f65f28949c0de6c4efd5a34fc49dd43387d9fddb
mikeldopacio <keldopacio@gmail.com>
2025-06-10 17:22:05 +0200

    preuba

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //detectar movil
 
-//Lazy loading
-cambiar("boton_2d");
-cambiar("boton_3d");
-cambiar("boton_storyboard");
-cambiar("boton_reel");
+//Lazy loading no funciona asi
+// cambiar("boton_2d");
+// cambiar("boton_3d");
+// cambiar("boton_storyboard");
+// cambiar("boton_reel");
 cambiar("boton_info");
 
 let main_path = "https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/main";
@@ -21,77 +21,143 @@ function cambiar(id){
 //Hay que recuperar el path de cada tree de forma dinámica o si no no se van a actualizar los contenidos subidos sino que va a usar lo sde cada comit anterior.
 
 function getCuerpo(id){
-            $.getJSON("https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/main", function(data) {
-                console.log(data["tree"][1]["path"]);
-                console.log(data["tree"][1]["url"]);
-                // current_path = data["tree"][1]["url"];
-                $.getJSON(data["tree"][1]["url"], function(data2) {
-                    console.log(data2["tree"][1]["path"]);
-                    console.log(data2["tree"][1]["url"]);
-                });
-            });
-    if(id=="boton_2d"){
-        $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/8831ec23e1a8d617a8dd3aedcc8c92935c891a9f', function(data) {
-            r= "";
-            for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
-                let ruta = data["tree"][i]["path"];
-                let extension = ruta.substr(ruta.length - 3);
-                if(extension == "mp4"){
-                    //video
-                    r = r + "<video controls class=video_portfolio><source src='imagenes/2D/"+ruta+"' type='video/mp4'>";
+    $.getJSON("https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/main", function(data) {
+        console.log(data["tree"][1]["path"]);
+        console.log(data["tree"][1]["url"]);
+        $.getJSON(data["tree"][1]["url"], function(data2) {
+            console.log(data2["tree"][1]["path"]);
+            console.log(data2["tree"][1]["url"]);
+                if(id=="boton_2d"){
+                    $.getJSON(data2["tree"][1]["url"], function(data3) {
+                        r= "";
+                        for(let i = 0; i < Object.keys(data3["tree"]).length; i++) {
+                            let ruta = data3["tree"][i]["path"];
+                            let extension = ruta.substr(ruta.length - 3);
+                            if(extension == "mp4"){
+                                //video
+                                r = r + "<video controls class=video_portfolio><source src='imagenes/2D/"+ruta+"' type='video/mp4'>";
+                            }
+                            else{
+                                //imagen
+                                r = r + "<img class=imagen_portfolio src='imagenes/2D/"+ruta+"'>";
+                            }
+                        }
+                        document.getElementById("cuerpo").innerHTML = r;
+                    });
+                }
+                else if(id=="boton_3d"){
+                    $.getJSON(data2["tree"][2]["url"], function(data3) {
+                        r= "";
+                        for(let i = 0; i < Object.keys(data3["tree"]).length; i++) {
+                            let ruta = data3["tree"][i]["path"];
+                            let extension = ruta.substr(ruta.length - 3);
+                            if(extension == "mp4"){
+                                //video
+                                r = r + "<video controls class=video_portfolio><source src='imagenes/3D/"+ruta+"' type='video/mp4'>";
+                            }
+                            else{
+                                //imagen
+                                r = r + "<img class=imagen_portfolio src='imagenes/3D/"+ruta+"'>";
+                            }
+                        }
+                    document.getElementById("cuerpo").innerHTML = r;
+                    });
+                }
+                else if(id=="boton_reel"){
+                    document.getElementById("cuerpo").innerHTML = "<iframe class=video_embedido src='https://www.youtube.com/embed/NsCCsxgjxFQ'></iframe>";
+                }
+                else if(id=="boton_storyboard"){
+                    $.getJSON(data2["tree"][3]["url"], function(data3) {
+                        r= "";
+                        for(let i = 0; i < Object.keys(data3["tree"]).length; i++) {
+                            let ruta = data3["tree"][i]["path"];
+                            let extension = ruta.substr(ruta.length - 3);
+                            if(extension == "mp4"){
+                                //video
+                                r = r + "<video controls class=video_portfolio><source src='imagenes/Storyboard/"+ruta+"' type='video/mp4'>";
+                            }
+                            else{
+                                //imagen
+                                r = r + "<img class=imagen_portfolio src='imagenes/Storyboard/"+ruta+"'>";
+                            }
+                        }
+                        document.getElementById("cuerpo").innerHTML = r;
+                    });
                 }
                 else{
-                    //imagen
-                    r = r + "<img class=imagen_portfolio src='imagenes/2D/"+ruta+"'>";
+                    document.getElementById("cuerpo").innerHTML = `
+                        Hey there! I'm Mikel. A 2D and 3D animator from Barcelona, Spain. I've studied for 4 years at La Salle Animation & VFX.
+                        I've worked at Nomada Studio during 6 months, doing clean up of videogame 2D animations. I've also been doing art comissions for 6 years.
+                        I strive to keep growing in my animation journey, thank you for your time.
+                    `;
                 }
-            }
-            document.getElementById("cuerpo").innerHTML = r;
+
         });
-    }
-    else if(id=="boton_3d"){
-                $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/8101248296cedcbd25e729a1a086bae376abf8b3', function(data) {
-            r= "";
-            for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
-                let ruta = data["tree"][i]["path"];
-                let extension = ruta.substr(ruta.length - 3);
-                if(extension == "mp4"){
-                    //video
-                    r = r + "<video controls class=video_portfolio><source src='imagenes/3D/"+ruta+"' type='video/mp4'>";
-                }
-                else{
-                    //imagen
-                    r = r + "<img class=imagen_portfolio src='imagenes/3D/"+ruta+"'>";
-                }
-            }
-            document.getElementById("cuerpo").innerHTML = r;
-        });
-    }
-    else if(id=="boton_reel"){
-        document.getElementById("cuerpo").innerHTML = "<iframe class=video_embedido src='https://www.youtube.com/embed/NsCCsxgjxFQ'></iframe>";
-    }
-    else if(id=="boton_storyboard"){
-                $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/5967120ca116056da6efc20847bdd5d2ee838e88', function(data) {
-            r= "";
-            for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
-                let ruta = data["tree"][i]["path"];
-                let extension = ruta.substr(ruta.length - 3);
-                if(extension == "mp4"){
-                    //video
-                    r = r + "<video controls class=video_portfolio><source src='imagenes/Storyboard/"+ruta+"' type='video/mp4'>";
-                }
-                else{
-                    //imagen
-                    r = r + "<img class=imagen_portfolio src='imagenes/Storyboard/"+ruta+"'>";
-                }
-            }
-            document.getElementById("cuerpo").innerHTML = r;
-        });
-    }
-    else{
-        document.getElementById("cuerpo").innerHTML = `
-            Hey there! I'm Mikel. A 2D and 3D animator from Barcelona, Spain. I've studied for 4 years at La Salle Animation & VFX.
-            I've worked at Nomada Studio during 6 months, doing clean up of videogame 2D animations. I've also been doing art comissions for 6 years.
-            I strive to keep growing in my animation journey, thank you for your time.
-        `;
-    }
+    });
+
 }
+
+    // if(id=="boton_2d"){
+    //     $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/8831ec23e1a8d617a8dd3aedcc8c92935c891a9f', function(data) {
+    //         r= "";
+    //         for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
+    //             let ruta = data["tree"][i]["path"];
+    //             let extension = ruta.substr(ruta.length - 3);
+    //             if(extension == "mp4"){
+    //                 //video
+    //                 r = r + "<video controls class=video_portfolio><source src='imagenes/2D/"+ruta+"' type='video/mp4'>";
+    //             }
+    //             else{
+    //                 //imagen
+    //                 r = r + "<img class=imagen_portfolio src='imagenes/2D/"+ruta+"'>";
+    //             }
+    //         }
+    //         document.getElementById("cuerpo").innerHTML = r;
+    //     });
+    // }
+    // else if(id=="boton_3d"){
+    //             $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/8101248296cedcbd25e729a1a086bae376abf8b3', function(data) {
+    //         r= "";
+    //         for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
+    //             let ruta = data["tree"][i]["path"];
+    //             let extension = ruta.substr(ruta.length - 3);
+    //             if(extension == "mp4"){
+    //                 //video
+    //                 r = r + "<video controls class=video_portfolio><source src='imagenes/3D/"+ruta+"' type='video/mp4'>";
+    //             }
+    //             else{
+    //                 //imagen
+    //                 r = r + "<img class=imagen_portfolio src='imagenes/3D/"+ruta+"'>";
+    //             }
+    //         }
+    //         document.getElementById("cuerpo").innerHTML = r;
+    //     });
+    // }
+    // else if(id=="boton_reel"){
+    //     document.getElementById("cuerpo").innerHTML = "<iframe class=video_embedido src='https://www.youtube.com/embed/NsCCsxgjxFQ'></iframe>";
+    // }
+    // else if(id=="boton_storyboard"){
+    //             $.getJSON('https://api.github.com/repos/mikeldopacio/mikelanimation/git/trees/5967120ca116056da6efc20847bdd5d2ee838e88', function(data) {
+    //         r= "";
+    //         for(let i = 0; i < Object.keys(data["tree"]).length; i++) {
+    //             let ruta = data["tree"][i]["path"];
+    //             let extension = ruta.substr(ruta.length - 3);
+    //             if(extension == "mp4"){
+    //                 //video
+    //                 r = r + "<video controls class=video_portfolio><source src='imagenes/Storyboard/"+ruta+"' type='video/mp4'>";
+    //             }
+    //             else{
+    //                 //imagen
+    //                 r = r + "<img class=imagen_portfolio src='imagenes/Storyboard/"+ruta+"'>";
+    //             }
+    //         }
+    //         document.getElementById("cuerpo").innerHTML = r;
+    //     });
+    // }
+    // else{
+    //     document.getElementById("cuerpo").innerHTML = `
+    //         Hey there! I'm Mikel. A 2D and 3D animator from Barcelona, Spain. I've studied for 4 years at La Salle Animation & VFX.
+    //         I've worked at Nomada Studio during 6 months, doing clean up of videogame 2D animations. I've also been doing art comissions for 6 years.
+    //         I strive to keep growing in my animation journey, thank you for your time.
+    //     `;
+    // }
